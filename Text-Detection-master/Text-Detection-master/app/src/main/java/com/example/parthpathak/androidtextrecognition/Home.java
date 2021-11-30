c265c87a458cf39853e212d503e21a383f6dc8be
Kowshique Roy <34647424+kowshiqueroy@users.noreply.github.com>
2021-12-01 02:23:46 +0600

    Initial commit

---
+++ Text-Detection-master/Text-Detection-master/app/src/main/java/com/example/parthpathak/androidtextrecognition/Home.java
@@ -0,0 +1,29 @@
+package com.example.parthpathak.androidtextrecognition;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+Button scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        scan=findViewById(R.id.button1);
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Main = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(Main);
+            }
+        });
+
+
+
+    }
+}
